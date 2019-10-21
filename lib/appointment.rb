404fe8143e3c785935a98aeaa7ebf86bf34f8aab
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 03:50:17 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -7,3 +7,9 @@ class Appointment
     patient.add_appointment(self) 
   end 
 end
+
+
+ferris = Patient.new("Ferris Bueller")
+doogie = Doctor.new("Doogie Howser")
+ 
+new_appointment = Appointment.new('January 1st, 1989', ferris, doogie)
